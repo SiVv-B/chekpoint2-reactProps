6474c75c0a345ced26a2fb645230638a47395e85
SiVv-B <siwar.blk@gmail.com>
2021-10-14 19:38:29 +0200

    second try chekpoint props 2

--- reactprops2/src/App.js
+++ reactprops2/src/App.js
@@ -1,29 +1,28 @@
-import React from "react";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import './App.css';
-import Profile from './Profile/Profile';
-import ProfilePicture from "./ProfilePicture.jpg";
+import React from 'react'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './App.css'
+import Profile from './Profile/Profile'
+import ProfilePicture from './ProfilePicture.jpg'
 
-toast.configure();
+toast.configure()
 function App() {
   const notify = () => {
-    toast("Hello Siwar! Welcome to your profile!");
-  };
+    toast('Hello Siwar! Welcome to your profile!')
+  }
   return (
     <div className="App">
       <Profile
-      FullName='Siwar Belkhir'
-      Bio='I am a student at GoMyCode.'
-      Profession='I am a student'
-      Pic={ProfilePicture}
-      >
-      </Profile>
-      <button onClick={notify} style={{ marginLeft: "18%" }}>
+        FullName="Siwar Belkhir"
+        Bio="I am a student at GoMyCode."
+        Profession="I am a student"
+        Pic={ProfilePicture}
+      ></Profile>
+      <button onClick={notify} style={{ marginLeft: '18%' }}>
         Click me
       </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- reactprops2/src/Profile/Profile.js
+++ reactprops2/src/Profile/Profile.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import propTypes from "prop-types";
-
+import React from 'react'
+import propTypes from 'prop-types'
 
 function Profile(props) {
-    return (
-        <div>
-            <h1> {props.FullName}</h1>
-            <p>{props.Bio}</p>
-            <h2>{props.Profession}</h2>
-        <img src={props.Pic} alt ='my rofiles picture'/>
-        </div>
-    )
+  return (
+    <div>
+      <h1> {props.FullName}</h1>
+      <p>{props.Bio}</p>
+      <h2>{props.Profession}</h2>
+      <img src={props.Pic} alt="my rofiles picture" />
+    </div>
+  )
 }
 
-Profile.propTypes ={
-    FullName: propTypes.string,
-    Bio: propTypes.string,
-    Profession: propTypes.string,
+Profile.propTypes = {
+  FullName: propTypes.string,
+  Bio: propTypes.string,
+  Profession: propTypes.string,
 }
 
-
-export default Profile;
-
-
+export default Profile
