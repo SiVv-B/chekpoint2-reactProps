019ab4d2519eb49f43cd10c5d946fca629fd3dfd
SiVv-B <siwar.blk@gmail.com>
2021-10-14 19:32:35 +0200

    Second Try chekpoint react props

---
+++ reactprops2/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import './App.css';
+import Profile from './Profile/Profile';
+import ProfilePicture from "./ProfilePicture.jpg";
+
+toast.configure();
+function App() {
+  const notify = () => {
+    toast("Hello Siwar! Welcome to your profile!");
+  };
+  return (
+    <div className="App">
+      <Profile
+      FullName='Siwar Belkhir'
+      Bio='I am a student at GoMyCode.'
+      Profession='I am a student'
+      Pic={ProfilePicture}
+      >
+      </Profile>
+      <button onClick={notify} style={{ marginLeft: "18%" }}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactprops2/src/Profile/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import propTypes from "prop-types";
+
+
+function Profile(props) {
+    return (
+        <div>
+            <h1> {props.FullName}</h1>
+            <p>{props.Bio}</p>
+            <h2>{props.Profession}</h2>
+        <img src={props.Pic} alt ='my rofiles picture'/>
+        </div>
+    )
+}
+
+Profile.propTypes ={
+    FullName: propTypes.string,
+    Bio: propTypes.string,
+    Profession: propTypes.string,
+}
+
+
+export default Profile;
+
+
